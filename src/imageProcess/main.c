87bbf0f486e45d56eadfcd20c92407ef2a63733b
Nicolas Prevost <nicolas.prevost@epita.fr>
2021-09-15 11:27:41 +0200

    [+] loard image from main

--- src/imageProcess/main.c
+++ src/imageProcess/main.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 #include "image.h"
 
+//picture path "/home/nicolas/Desktop/OCR/tests/assets/image_02.jpeg"
+
 int main(){
-	SDL_Surface* picture = load("/home/nicolas/Desktop/OCR/tests/assets/image_01.jpeg");
+	SDL_Surface* picture = load("tests/assets/image_02.jpeg");
 	if(!picture){
 		printf("error in load\n");
 	}
 	else{
-		printf("image loaded succesfully\n");
+		printf("picture loaded succefully!\n");
 	}
+
+
+	printf("\nlargeur:%d hauteur:%d\n", picture->w, picture->h);
 	return 0;
 }
