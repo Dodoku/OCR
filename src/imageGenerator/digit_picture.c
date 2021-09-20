fe78b063271bd3de3576735399f796273c8b9634
Nicolas Prevost <nicolas.prevost@epita.fr>
2021-09-20 20:01:51 +0200

    [+]image Generator done

--- src/imageGenerator/digit_picture.c
+++ src/imageGenerator/digit_picture.c
@@ -1,56 +1,59 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_pixels.h>
-#include "digit_draw.h"
 #include "wall_draw.h"
 #include "../imageProcess/image.h"
 
 void display_digit(SDL_Surface* image, int x, int y, int n){
+	SDL_Surface* number;
 	switch(n){
 		case 1:
-			one(image,x,y);
+			number = load("src/imageGenerator/assets/one.jpg");
 			break;
 		case 2:
-			two(image,x,y);
+			number = load("src/imageGenerator/assets/two.jpg");		
 			break;
 		case 3:
-			three(image,x,y);
+			number = load("src/imageGenerator/assets/three.jpg");
 			break;
 		case 4:
-			four(image,x,y);
+			number = load("src/imageGenerator/assets/four.jpg");
 			break;
 		case 5:
-			five(image,x,y);
+			number = load("src/imageGenerator/assets/five.jpg");
 			break;
 		case 6:
-			six(image,x,y);
+			number = load("src/imageGenerator/assets/six.jpg");
 			break;
 		case 7:
-			seven(image,x,y);
+			number = load("src/imageGenerator/assets/seven.jpg");
 			break;
 		case 8:
-			eight(image,x,y);
+			number = load("src/imageGenerator/assets/eight.jpg");
 			break;
 		case 9:
-			nine(image,x,y);
+			number = load("src/imageGenerator/assets/nine.jpg");
 			break;
 		default:
 			break;
 	}
+	for(int i=0;i<100;i++){
+		for(int j=0;j<100;j++){
+			set_pixel(image,x+i,y+j,get_pixel(number,i,j));
+		}
+	}
+
 }
 
 void generate_digit_picture(int sudoku[], char* path[]){
-	SDL_Surface* image = create_empty(72, 99);
+	SDL_Surface* image = create_empty(993, 993);
 
-	horizontal_wall(image,0);
-	horizontal_wall(image,1);
-	horizontal_wall(image,2);
-	vertical_wall(image,0,3);
-	vertical_wall(image,1,3);
-	vertical_wall(image,2,3);
+	horizontal_wall(image,0,12);
+	vertical_wall(image,0,12,12);
 
 
-	int x = 3;
-	int y = 3;
+
+	int x = 13;
+	int y = 12;
 	int horizontal = 0;
 	int vertical = 0;
 
@@ -60,39 +63,32 @@ void generate_digit_picture(int sudoku[], char* path[]){
 
 		if(horizontal==3 || horizontal==6 || horizontal==9){
 			if(horizontal==9){
-				vertical_wall(image,x+6,y);
-				vertical_wall(image,x+7,y);
-				vertical_wall(image,x+8,y);
-				x=3;
+				vertical_wall(image,x+101,y,12);
+				x=13;
 				vertical++;
 				if(vertical==3 || vertical==6 || vertical==9){
-					horizontal_wall(image,y+9);
-					horizontal_wall(image,y+10);
-					horizontal_wall(image,y+11);
-					y+=12;
+					horizontal_wall(image,y+101,12);
+					y+=113;
 					horizontal=0;
 				}
 				else{
-					horizontal_wall(image,y+9);
-					y+=10;
+					horizontal_wall(image,y+101,6);
+
+					y+=107;
 					horizontal=0;
 				}
 				if(vertical!=9){
-					vertical_wall(image,0,y);
-				vertical_wall(image,1,y);
-				vertical_wall(image,2,y);
+					vertical_wall(image,0,y,12);
 				}
 			}
 			else{
-				vertical_wall(image,x+6,y);
-				vertical_wall(image,x+7,y);
-				vertical_wall(image,x+8,y);
-				x+=9;
+				vertical_wall(image,x+101,y,12);
+				x+=113;
 			}
 		}
 		else{
-			vertical_wall(image,x+6,y);
-			x+=7;
+			vertical_wall(image,x+101,y,6);
+			x+=107;
 		}
 
 		
--- src/imageGenerator/wall_draw.c
+++ src/imageGenerator/wall_draw.c
@@ -2,14 +2,19 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_pixels.h>
 
-void horizontal_wall(SDL_Surface* image,int y){
-	for(int i=0;i<72;i++){
-		set_pixel(image, i,y,to_color(100,100,100,255));
+void horizontal_wall(SDL_Surface* image,int y, int thickness){
+	for(int i=0;i<=993;i++){
+		for(int t=0;t<thickness;t++){
+			set_pixel(image, i,y+t,to_color(0,0,0,0));
+		}
+		
 	}
 }
 
-void vertical_wall(SDL_Surface* image, int x, int y){
-	for(int i=0;i<9;i++){
-		set_pixel(image, x,i+y,to_color(100,100,100,255));
+void vertical_wall(SDL_Surface* image, int x, int y, int thickness){
+	for(int i=0;i<=100;i++){
+		for(int t=0;t<thickness;t++){
+			set_pixel(image, x+t,i+y,to_color(0,0,0,0));
+		}
 	}
 }
