4d6fb6388b6a4ffb15b507f6126cd51cdad3be09
Nicolas Prevost <nicolas.prevost@epita.fr>
2021-10-17 02:12:50 +0200

    [*] Contrast to fix

--- src/imageProcess/main.c
+++ src/imageProcess/main.c
@@ -11,11 +11,14 @@
 
 int main() {
     char s[] = "./tests/assets/image_00.jpeg";
-    char s1[] = "0.jpeg";
+    char s1[] = "tests/generated/0.jpeg";
     for(int i = 1; i < 7; i++){
         s[22] = '0'+i;
-        s1[0] = '0'+i;
+        s1[16] = '0'+i;
         SDL_Surface* image = load(s);
+        //image = to_black_and_white(image);
+        //image = noise_reduction(image,0.5);
+        image = contrast(image,0.5);
         save(image, s1);
     }
 
