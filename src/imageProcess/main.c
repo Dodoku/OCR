fad6c4c5329390754f97b7a4694bd5c8ce199eae
Nicolas Prevost <nicolas.prevost@epita.fr>
2021-10-24 16:15:23 +0200

    [+] main for rotate, blur and greyscale

--- src/imageProcess/main.c
+++ src/imageProcess/main.c
@@ -2,6 +2,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_pixels.h>
+#include <err.h>
+#include <string.h>
 
 #include "../tools/image.h"
 #include "rotate.h"
@@ -9,19 +11,45 @@
 #include "greyScale.h"
 #include "noise_reduction.h"
 
-int main() {
-    char s[] = "./tests/assets/image_00.jpeg";
-    char s1[] = "tests/generated/0.jpeg";
-    for(int i = 1; i < 7; i++){
-        s[22] = '0'+i;
-        s1[16] = '0'+i;
-        SDL_Surface* image = load(s);
-        printf("je suis dylan et des fonctions a la con\n" );
-        image = to_greyscale(image);
-        image = gaussian_blur(image,3);
-
-        save(image, s1);
+int main(int argc, char *argv[]) {
+    if(argc == 1){
+        printf("Use greyscale + path-src + path-dest\n(to put a picture in grayscale)\n\n");
+        printf("Use rotate + path-src + path-dest + degree\n(to rotate a picture with a degree in degree)\n\n");
+        printf("Use blur + path-rc + path-dest + n\n(to apply the gaussian_blur n times)\n");
+        return 1;
     }
 
+    if(!strcmp(argv[1],"greyscale")){
+        if(argc != 4)
+            errx(1,"greyscale -> wrong number of argument\n");
+        SDL_Surface* image = load(argv[2]);
+        image = to_greyscale(image);
+        save(image, argv[3]);
+        printf("greyscale -> picture saved\n");
+        return 0;
+    }
+
+    if(!strcmp(argv[1],"rotate")){
+        if(argc != 5)
+            errx(1,"rotate -> wrong number of argument\n");
+        SDL_Surface* image = load(argv[2]);
+        image = rotate(image, -atoi(argv[4])*3.14/180);
+        save(image, argv[3]);
+        printf("rotate -> picture saved\n");
+        return 0;
+    }
+
+    if(!strcmp(argv[1],"blur")){
+        if(argc != 5)
+            errx(1,"blur -> wrong number of argument\n");
+        SDL_Surface* image = load(argv[2]);
+        image = gaussian_blur(image, atoi(argv[4]));
+        save(image, argv[3]);
+        printf("blur -> picture saved\n");
+        return 0;
+    }
+
+
+    printf("wrong argument\n");
     return 0;
 }
