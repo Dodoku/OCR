9e3c1e8d85635a9e3a9bce9b434c1e2cf02341fb
Nicolas Prevost <nicolas.prevost@epita.fr>
2021-10-24 18:26:23 +0200

    [+] final image process main

--- src/imageProcess/main.c
+++ src/imageProcess/main.c
@@ -8,24 +8,27 @@
 #include "../tools/image.h"
 #include "rotate.h"
 #include "contrast.h"
-#include "greyScale.h"
+#include "grayscale.h"
 #include "noise_reduction.h"
 
 int main(int argc, char *argv[]) {
     if(argc == 1){
+        printf("\n\nUse all + '-' + '-'\n(to apply all the filters on all the pictures)\n\n");
+        printf("Use all + path-src + path-dest\n(to apply all the filters)\n\n");
         printf("Use greyscale + path-src + path-dest\n(to put a picture in grayscale)\n\n");
         printf("Use rotate + path-src + path-dest + degree\n(to rotate a picture with a degree in degree)\n\n");
-        printf("Use blur + path-rc + path-dest + n\n(to apply the gaussian_blur n times)\n");
+        printf("Use blur + path-rc + path-dest + n\n(to apply the gaussian_blur n times)\n\n");
+        printf("Use otsu + path-rc + path-dest \n(to binarise the picture)\n\n");
         return 1;
     }
 
-    if(!strcmp(argv[1],"greyscale")){
+    if(!strcmp(argv[1],"grayscale")){
         if(argc != 4)
-            errx(1,"greyscale -> wrong number of argument\n");
+            errx(1,"grayscale -> wrong number of argument\n");
         SDL_Surface* image = load(argv[2]);
-        image = to_greyscale(image);
+        image = to_grayscale(image);
         save(image, argv[3]);
-        printf("greyscale -> picture saved\n");
+        printf("grayscale -> picture saved\n");
         return 0;
     }
 
@@ -46,6 +49,46 @@ int main(int argc, char *argv[]) {
         image = gaussian_blur(image, atoi(argv[4]));
         save(image, argv[3]);
         printf("blur -> picture saved\n");
+        return 0;
+    }
+
+    if(!strcmp(argv[1],"otsu")){
+        if(argc != 4)
+            errx(1,"otsu -> wrong number of argument\n");
+        SDL_Surface* image = load(argv[2]);
+        image = otsu(image);
+        save(image, argv[3]);
+        printf("otsu -> picture saved\n");
+        return 0;
+    }
+
+    if(!strcmp(argv[1],"all")){
+        if(argc != 4)
+            errx(1,"all -> wrong number of argument\n");
+        if(!strcmp(argv[2],"-")){
+            char s[] = "tests/assets/image_00.jpeg";
+            char s1[] = "0.jpeg";
+            for(int i = 1; i < 7; i++){
+                s[20] = '0'+i;
+                s1[0] = '0'+i;
+                SDL_Surface* image = load(s);
+                image = to_grayscale(image);
+                image = otsu(image);
+
+                save(image, s1);
+                printf("generate image -> %s\n",s1);
+            }
+        }
+        else{
+            SDL_Surface* image = load(argv[2]);
+            image = to_grayscale(image);
+            //image = gaussian_blur(image, 1);
+            //image = simple_blur(image,2);
+            image = otsu(image);
+            save(image, argv[3]);
+            printf("all -> picture saved\n");
+            return 0;
+        }
         return 0;
     }
 
