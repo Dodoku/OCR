e0f13b9fead00fa4fba03fe09c3043c7637bc0a9
Valentin Uhlrich <valentin.uhlrich@epita.fr>
2021-11-11 22:41:47 +0100

    hough test

--- src/imageSpliter/hough.c
+++ src/imageSpliter/hough.c
@@ -7,17 +7,30 @@
 
 #include "../tools/image.h"
 
-#define tmax 360
+#define tmax 179
 
-int** init_matrice(size_t height, size_t width){
-	int **A = malloc(sizeof(int*[height]));
+size_t calc_rhomax(SDL_Surface* image){
+    return (size_t) sqrt(image->w * image->w + image->h * image->h);
+}
+
+int get_value(int **A, size_t rhomax, unsigned int theta, int rho){
+    return A[theta][(size_t) (rho+rhomax)];
+}
+
+void set_value(int **A, size_t rhomax, unsigned int theta, int rho,
+                                                                int value){
+    A[theta][(size_t) (rho+rhomax)] = value;
+}
+
+int** init_matrice(size_t rhomax){
+	int **A = malloc(sizeof(int*[tmax]));
     
-    for (size_t i = 0; i < height; i++){
-        A[i] = malloc(sizeof(int[width]));
+    for (size_t i = 0; i < tmax; i++){
+        A[i] = malloc(sizeof(int[rhomax*2]));
     }
 
-	for (size_t i = 0; i < height; i++){
-		for (size_t j = 0; j < width ; j++){
+	for (size_t i = 0; i < tmax; i++){
+		for (size_t j = 0; j < rhomax*2; j++){
 			A[i][j] = 0;
 		}
 	}
@@ -25,8 +38,8 @@ int** init_matrice(size_t height, size_t width){
 	return A;
 }
 
-void free_matrice(int** A, size_t height){
-	for (size_t i = 0; i < height; i++)
+void free_matrice(int** A){
+	for (size_t i = 0; i < tmax; i++)
 		free(A[i]);
 
 	free(A);
@@ -35,7 +48,7 @@ void free_matrice(int** A, size_t height){
 int max(int** A, size_t rhomax){
     int max = 0;
     for(size_t i = 0; i < tmax; i++){
-        for(size_t j = 0; j < rhomax; j++){
+        for(size_t j = 0; j < rhomax*2; j++){
             if(A[i][j] > max){
                 max = A[i][j];
             }
@@ -44,7 +57,7 @@ int max(int** A, size_t rhomax){
     return max;
 }
 
-SDL_Surface* hough_print(int**A, size_t rhomax){
+/*SDL_Surface* hough_print(int**A, size_t rhomax){
     SDL_Surface* output = create_empty(tmax, rhomax);
     int valmax = max(A,rhomax);
     Uint8 value;
@@ -55,7 +68,7 @@ SDL_Surface* hough_print(int**A, size_t rhomax){
 	}
     }
     return output;
-}
+}*/
 
 void line_trace(SDL_Surface* input, double theta, double rho){
     size_t height = input->h, width = input->w;
@@ -63,49 +76,48 @@ void line_trace(SDL_Surface* input, double theta, double rho){
     size_t i, j;
     for (double x = 0; x < width; x++){
         y = rho - x*cos(theta);
-	y /= sin(theta);
-	if (x>0 && y>0){
-            i = (size_t) x, j = (size_t) y;
-	    if (i < height && j < width){
-                set_pixel(input, j, i, to_color(255,0,0,255));
-	    }
-	}
+        y /= sin(theta);
+        if (x>0 && y>0){
+                i = (size_t) x, j = (size_t) y;
+                if (i < width && j < height){
+                        set_pixel(input, i, j, to_color(255,0,0,255));
+                }
+        }
     }
 }
 
-void hough_trace(int** A, double x, double y, double rhomax){
+void hough_trace(int** A, double x, double y, size_t rhomax){
     double rho, theta;
     for(size_t t = 0; t < tmax; t++){
         theta = ((double) t)*(M_PI/180);
-        rho = (rhomax/2) + x*cos(theta) + y*sin(theta);
-        A[t][(size_t) rho] += 1;
+        rho = x*cos(theta) + y*sin(theta);
+        set_value(A, rhomax, theta, rho, get_value(A, rhomax, theta, rho) + 1);
     }
 }
 
-SDL_Surface* hough_transform(SDL_Surface* input){
+void hough_transform(SDL_Surface* input){
     size_t height = input->h, width = input->w;
-    size_t rhomax = 2*(height+width);
-    int** A = init_matrice(tmax, rhomax);
+    size_t rhomax = calc_rhomax(input);
+    int** A = init_matrice(rhomax);
 
-    for(size_t i = 0; i < height; i++){
-        for(size_t j = 0; j < width; j++){
-            if(get_pixel(input, i, j).r > 0)
-                hough_trace(A, (double) i, (double) j, (double) rhomax);
+    for(size_t i = 0; i < width; i++){
+        for(size_t j = 0; j < height; j++){
+            if(get_pixel(input, i, j).r < 255/2)
+                hough_trace(A, (double) i, (double) j, rhomax);
         }
-	//printf("line : %zu\n",i);
     }
 
-    return hough_print(A,rhomax);/*
     int threshold = max(A, rhomax);
-    threshold = (3 * threshold)/4;
+    threshold /= 2;
+
     printf("threshold = %i\n", threshold);
-    for(size_t x = 1; x < tmax; x++){
-	for(size_t y = 0; y < rhomax; y++){
-	    if (A[x][y] > threshold)
-		line_trace(input, (double) x, (double) (y-rhomax/2));
-	}
+
+    for(unsigned int t = 0; t < tmax; t++){
+        for(int r = -rhomax; r < (int) rhomax; r++){
+            if (get_value(A, rhomax, t, r) > threshold)
+                line_trace(input, (double) t, (double) r);
+        }
     }
     printf("threshold = %i\n", threshold);
-    free_matrice(A,tmax);
-    return input;*/
+    free_matrice(A);
 }
