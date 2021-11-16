30da30735af44e585535de24dbd3de386cb16f0b
nico <nicolas.prevost@epita.fr>
2021-11-16 21:59:33 +0100

    [+] single split working!

--- src/imageSpliter/main.c
+++ src/imageSpliter/main.c
@@ -4,48 +4,18 @@
 #include <SDL2/SDL_pixels.h>
 
 #include "hough.h"
+#include "spliter.h"
 #include "../tools/image.h"
 
 int main(int argc, char *argv[])
 {
-    char s[] = "tests/assets/image_00.jpeg";
-    char s1[] = "0.jpeg";
-    for(int i = 1; i < 7; i++)
-    {
-        s[20] = '0'+i;
-        s1[0] = '0'+i;
-        SDL_Surface* image = load(s);
-        image = to_grayscale(image);
-        image = otsu(image);
+    char s[] = "tests/assets/image_01.jpeg";
+    SDL_Surface* image = load(s);
+    SDL_Surface* ret;
+    ret = split(image,333,333,666,666);
 
-        save(image, s1);
-        printf("generate image -> %s\n",s1);
-    }
+    save(ret, "splited.jpeg");
+    printf("image saved");
+
     return 0;
 }
-
-/*
-int main() {
-    SDL_Surface* image = load("tests/assets/imageGrey_01.jpeg");
-    hough_transform(image);
-    return 0;
-}
-
-void test_matrix(){
-    int** A = init_matrice(10,8);
-    for (size_t i = 0; i < 10; i++)
-        A[i][1] = 1;
-    print_matrice(A,10,8);
-    free_matrice(A,10);
-    return 0;
-}
-
-void test_SDL(){
-    SDL_Surface* print = create_empty(100, 80);
-    for (size_t i = 0; i < 100; i++){
-        set_pixel(print, i, 1, to_color(0, 0, 0, 255));
-    }
-    save(print, "tests/assets/sinus.jpeg");
-    return 0;
-}
-*/
--- src/imageSpliter/spliter.c
+++ src/imageSpliter/spliter.c
@@ -1,5 +1,13 @@
 #include "../tools/image.h"
 
+/*
+ * crop the picture at (x1, y1) (x2,y2)
+ * @param image (SDL_Surface*) : the picture to crop
+ * @param x1,y1 (int) : coordonates at the right top corner
+ * @param x2,y2 (int) : coordonates at the bottom left corner
+ * @return (SDL_Surface*) : return the splited picture
+ * @author Nicolas Prevost
+ */
 
 SDL_Surface* split(SDL_Surface* image, int x1, int y1, int x2, int y2)
 {
