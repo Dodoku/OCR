fbf6ac7931a61ebf3758e1429e96eef59ab9ca83
nico <nicolas.prevost@epita.fr>
2021-12-04 20:19:22 +0100

    [+] red collors in picture insert

--- src/imageGenerator/main.c
+++ src/imageGenerator/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "digit_picture.h"
 #include <string.h>
+#include "../tools/image.h"
 
 int main(int argc, char *argv[]) {
     if(argc == 1){
@@ -9,10 +10,14 @@ int main(int argc, char *argv[]) {
     if(argc == 2){
 
         char out[2048];
+
+        char og[81]="....458....721..34.3......21..67..4.7....2..63..49..13.6.........158..6.....695.";
+        char sol[81]="27634589589721643463985127218567394974813265635249871356492718792158436841376952";
         strcpy(out, argv[1]);
         strcat(out, ".jpeg");
 
-        generate_digit_picture(argv[1], out);
+        SDL_Surface* image = generate_digit_picture(og, sol);
+        save(image,out);
         printf("Picture generated and saved as \"%s\"\n", out);
     }
     else{
